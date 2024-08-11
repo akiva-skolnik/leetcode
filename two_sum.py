0020755a8094b97ebdd7b111364a2d79b70a301d
akiva0003 <akiva0003@gmail.com>
2024-08-11 21:55:52 +0300

    two-sum another solution

--- two_sum.py
+++ two_sum.py
@@ -4,10 +4,18 @@ from typing import List
 # https://leetcode.com/problems/two-sum
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return next([i, j]
-                    for i in range(len(nums))
-                    for j in range(i + 1, len(nums))
-                    if nums[i] + nums[j] == target)
+        """Given an array of integers nums and an integer target,
+            return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order."""
+        seen = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff not in seen:
+                seen[nums[i]] = i
+            else:
+                return [seen[diff], i]
+
 
 
 def test_1():
