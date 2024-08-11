09eccc19836a8d4313c08cbba24a7b80bf14315c
akiva0003 <akiva0003@gmail.com>
2024-08-11 19:10:23 +0300

    remove-element

---
+++ remove_element.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+# https://leetcode.com/problems/remove-element
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        n = len(nums)
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+                i += 1
+
+        return i
+
+
+def test():
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    assert Solution().removeElement(nums, 2) == 5
+    assert nums == [0, 1, 3, 0, 4]
