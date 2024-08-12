816323934e85a87b7d084f2af9a04c56950179cb
akiva0003 <akiva0003@gmail.com>
2024-08-12 03:43:14 +0300

    add-binary seconds solution

--- add_binary.py
+++ add_binary.py
@@ -1,8 +1,25 @@
 # https://leetcode.com/problems/add-binary
 class Solution:
-    def addBinary(self, a: str, b: str) -> str:
+    def addBinary1(self, a: str, b: str) -> str:
         """Given two binary strings a and b, return their sum as a binary string."""
         return bin(int(a, 2) + int(b, 2))[2:]
+
+    def addBinary(self, a: str, b: str) -> str:
+        """Given two binary strings a and b, return their sum as a binary string."""
+        result = []
+        carry = 0
+        i, j = len(a) - 1, len(b) - 1
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            result.append(str(total % 2))
+            carry = total // 2
+        return "".join(result[::-1])
 
 
 def test_1():
