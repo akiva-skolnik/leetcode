8d7f8d5857112a32885034ac10fa46cb0ea6d733
akiva <akiva.skolnik@gmail.com>
2024-08-20 06:02:22 +0300

    plus-one

---
+++ plus_one.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+# https://leetcode.com/problems/plus-one
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        """Increment the integer (represented by digits) by one and return the resulting array of digits."""
+        i = len(digits) - 1
+        while i >= 0:
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                break
+            i -= 1
+        if i < 0:
+            digits.insert(0, 1)
+        return digits
+
+
+def test_1():
+    assert Solution().plusOne([1, 2, 3]) == [1, 2, 4]
+
+
+def test_2():
+    assert Solution().plusOne([4, 3, 2, 1]) == [4, 3, 2, 2]
+
+
+def test_3():
+    assert Solution().plusOne([9]) == [1, 0]
+
+
+def test_4():
+    assert Solution().plusOne([0]) == [1]
