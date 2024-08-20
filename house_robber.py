04a5b7beac13d739374d5a0bdaa4220bf49d7018
akiva <akiva.skolnik@gmail.com>
2024-08-20 05:45:22 +0300

    house-robber faster solution (dynamic programming)

--- house_robber.py
+++ house_robber.py
@@ -5,11 +5,28 @@ from typing import List
 # https://leetcode.com/problems/house-robber
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        """Given an integer array nums representing the amount of money of each house,
+            return the maximum amount of money you can rob without robbing two adjacent houses."""
+        # dynamic programming - O(n) solution
+        if len(nums) <= 2:
+            return max(nums or [0])
+        # history[i] = max rob from i to n
+        history = [0] * len(nums)
+        history[-1] = nums[-1]
+        history[-2] = max(nums[-1], nums[-2])
+        for i in range(len(nums) - 3, -1, -1):
+            # we better rob current house and forget about the prv.
+            history[i] = max(history[i + 1], history[i + 2] + nums[i])
+
+        return history[0]
+
+    def rob_2(self, nums: List[int]) -> int:
         self.nums = nums
         return self.rob_rec(-2, 0)
 
     @lru_cache
     def rob_rec(self, last_i: int, i: int) -> int:
+        # n^2 solution
         if i == len(self.nums):
             return 0
         if i == last_i + 1:
