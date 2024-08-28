2d052671d52fdf30e65db7cfc727425362593712
akiva <akiva.skolnik@gmail.com>
2024-08-28 06:39:18 +0300

    valid-anagram - better solution

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,6 +1,12 @@
+from collections import Counter
+
+
 # https://leetcode.com/problems/valid-anagram
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)  # O(n)
+
+    def isAnagram1(self, s: str, t: str) -> bool:
         """Given two strings s and t, return true if by rearranging the letters of s, it can be made equal to t."""
         return sorted(s) == sorted(t)  # O(n log n)
 
