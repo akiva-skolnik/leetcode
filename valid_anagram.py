8b134606a332cbb6e226df0e9168f7d34d13fe8d
akiva <akiva.skolnik@gmail.com>
2024-08-28 06:37:01 +0300

    valid-anagram

---
+++ valid_anagram.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/valid-anagram
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """Given two strings s and t, return true if by rearranging the letters of s, it can be made equal to t."""
+        return sorted(s) == sorted(t)  # O(n log n)
+
+
+def test_solution():
+    assert Solution().isAnagram("anagram", "nagaram") is True
+    assert Solution().isAnagram("rat", "car") is False
+    assert Solution().isAnagram("a", "a") is True
+    assert Solution().isAnagram("a", "b") is False
+    assert Solution().isAnagram("a", "ab") is False
+    assert Solution().isAnagram("ab", "a") is False
+    assert Solution().isAnagram("ab", "ba") is True
+    assert Solution().isAnagram("abc", "bca") is True
