71afde49b99d76b5a483d7d5674286c099d1fb81
akiva <akiva.skolnik@gmail.com>
2024-08-30 16:08:51 +0300

    roman-to-integer another solution

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -12,6 +12,13 @@ class Solution:
                 result += romans[s[i]]
         return result
 
+    def romanToInt2(self, s: str) -> int:
+        romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace(
+            "XL", "XXXX").replace("XC", "LXXXX").replace(
+            "CD", "CCCC").replace("CM", "DCCCC")
+        return sum(romans[c] for c in s)
+
 
 def test_1():
     s = "III"
